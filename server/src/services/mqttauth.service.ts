9a7582f2b0ab00d97e9c7da8980e63a8596cb2a2
Christian Gahlert <1876381+chrisgahlert@users.noreply.github.com>
2026-01-22 07:31:02 +0100

    Separate topics by devices to improve security

--- server/src/services/mqttauth.service.ts
+++ server/src/services/mqttauth.service.ts
@@ -11,7 +11,9 @@ class MqttAuthService {
   private devices = deviceModel;
 
   public async user(authData: AuthUserDto): Promise<boolean> {
-    if (isEmpty(authData)) throw new HttpException(400, "You're not userData");
+    if (isEmpty(authData)) {
+      throw new HttpException(400, "You're not userData");
+    }
 
     if (authData.username == mqttclient.getUser() && authData.password == mqttclient.getPassword()) {
       return true;
@@ -19,14 +21,18 @@ class MqttAuthService {
 
     const findDevice: Device = await this.devices.findOne({ username: authData.username });
 
-    if (!findDevice) return false;
-    if (authData.password !== findDevice.password) return false;
+    if (!findDevice) {
+      console.log('mqtt-auth: device not found:', authData.username);
+      return false;
+    }
 
-    return true;
+    return authData.password === findDevice.password;
   }
 
   public async vhost(authData: AuthVhostDto): Promise<boolean> {
-    if (isEmpty(authData)) throw new HttpException(400, "You're not userData");
+    if (isEmpty(authData)) {
+      throw new HttpException(400, "You're not userData");
+    }
 
     if (authData.username == mqttclient.getUser()) {
       return true;
@@ -35,15 +41,17 @@ class MqttAuthService {
     const findDevice: Device = await this.devices.findOne({ username: authData.username });
 
     if (!findDevice) {
+      console.log('mqtt-auth: device not found:', authData.username);
       return false;
     }
-    if (authData.vhost !== '/') return false;
 
-    return true;
+    return authData.vhost === '/';
   }
 
   public async topic(authData: AuthTopicDto): Promise<boolean> {
-    if (isEmpty(authData)) throw new HttpException(400, "You're not userData");
+    if (isEmpty(authData)) {
+      throw new HttpException(400, "You're not userData");
+    }
 
     if (authData.username == mqttclient.getUser()) {
       return true;
@@ -51,16 +59,28 @@ class MqttAuthService {
 
     const findDevice: Device = await this.devices.findOne({ username: authData.username });
 
-    if (!findDevice) return false;
-    if (authData.resource !== 'topic') return false;
-    if (authData.name !== 'amq.topic') return false;
-    //if (! authData.routing_key.startsWith(`device.${findDevice.device_id}`)) throw new HttpException(403, "access denied");
+    if (!findDevice) {
+      console.log('mqtt-auth: device not found:', authData.username);
+      return false;
+    }
+    if (authData.resource !== 'topic') {
+      return false;
+    }
+    if (authData.name !== 'amq.topic') {
+      return false;
+    }
+    if (!authData.routing_key.startsWith(`.devices.${findDevice.device_id}.`)) {
+      console.log('mqtt-auth: routing key not allowed:', authData.routing_key);
+      throw new HttpException(403, 'access denied');
+    }
 
     return true;
   }
 
   public async resource(authData: AuthResourceDto): Promise<boolean> {
-    if (isEmpty(authData)) throw new HttpException(400, "You're not userData");
+    if (isEmpty(authData)) {
+      throw new HttpException(400, "You're not userData");
+    }
 
     if (authData.username == mqttclient.getUser()) {
       return true;
@@ -68,10 +88,24 @@ class MqttAuthService {
 
     const findDevice: Device = await this.devices.findOne({ username: authData.username });
 
-    if (!findDevice) return false;
-    if (authData.vhost !== '/') return false;
-    // if (authData.resource !== 'exchange') throw new HttpException(409, "access denied");
-    // if (authData.name !== 'amq.topic') throw new HttpException(409, "access denied");
+    if (!findDevice) {
+      console.log('mqtt-auth: device not found:', authData.username);
+      return false;
+    }
+    if (authData.vhost !== '/') {
+      return false;
+    }
+    if (authData.resource !== 'exchange') {
+      if (authData.resource === 'queue' && authData.name === `mqtt-subscription-${authData.client_id}qos0`) {
+        // needed for subscriptions
+        return true;
+      }
+
+      throw new HttpException(409, 'access denied');
+    }
+    if (authData.name !== 'amq.topic') {
+      throw new HttpException(409, 'access denied');
+    }
 
     return true;
   }
