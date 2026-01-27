febd825456ecab0a7abc28f8791cafabd767262d
chrisgahlert <1876381+chrisgahlert@users.noreply.github.com>
2026-01-28 00:10:59 +0100

    Add support for resizing images

--- server/src/controllers/image.controller.ts
+++ server/src/controllers/image.controller.ts
@@ -2,7 +2,31 @@ import { NextFunction, Response } from 'express';
 import { RequestWithUser } from '@/interfaces/auth.interface';
 import { isUserDeviceMiddelware } from '@/middlewares/auth.middleware';
 import { imageService } from '@services/image.service';
-import { readFile } from 'node:fs/promises'; // new import
+import { readFile, unlink } from 'node:fs/promises'; // new import
+import im from 'imagemagick';
+import { readFileSync } from 'fs';
+import { tmpdir } from 'node:os';
+import { join } from 'path';
+import { v4 as uuidv4 } from 'uuid';
+
+function arrayBufferToString(buffer) {
+  return binaryToString(String.fromCharCode.apply(null, Array.prototype.slice.apply(new Uint8Array(buffer))));
+}
+
+function binaryToString(binary) {
+  let error;
+
+  try {
+    return decodeURIComponent(escape(binary));
+  } catch (_error) {
+    error = _error;
+    if (error instanceof URIError) {
+      return binary;
+    } else {
+      throw error;
+    }
+  }
+}
 
 class ImageController {
   public getDeviceImage = async (req: RequestWithUser, res: Response, next: NextFunction) => {
@@ -18,7 +42,34 @@ class ImageController {
         if (image) {
           res.setHeader('Content-type', image.format === 'mp4' ? 'video/mp4' : 'image/jpeg');
           res.setHeader('Cache-Control', 'max-age=3600');
-          res.send(image.data);
+
+          if (req.query.width || req.query.height) {
+            const tmpFile = join(tmpdir(), req.params.device_id + '-' + Number(req.query.timestamp ?? Date.now()) + '-' + uuidv4() + '.jpeg');
+
+            im.resize(
+              {
+                srcData: arrayBufferToString(image.data),
+                dstPath: tmpFile,
+                width: Number(req.query.width ?? 0),
+                height: Number(req.query.height ?? 0),
+              },
+              async (err: any, buffer: any) => {
+                try {
+                  if (err) {
+                    console.log('Failed resizing image:', err);
+                    res.status(500).send(await readFile('assets/no-image_placeholder.png'));
+                  } else {
+                    console.log('Image resized successfully', typeof buffer, buffer.length, buffer);
+                    res.send(readFileSync(tmpFile));
+                  }
+                } finally {
+                  void unlink(tmpFile);
+                }
+              },
+            );
+          } else {
+            res.send(image.data);
+          }
         } else {
           res.setHeader('Cache-Control', 'no-cache');
           if (req.query.format === 'mp4') {
